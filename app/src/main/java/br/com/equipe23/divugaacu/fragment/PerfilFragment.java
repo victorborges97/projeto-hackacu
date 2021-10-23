2d5e986641d9a4286f2cf5af3bb9c7f6bfef17a7
Leonardo Pimentel <leozito4532@gmail.com>
2021-10-23 12:55:18 -0300

    tela perfil

--- app/src/main/java/br/com/equipe23/divugaacu/fragment/PerfilFragment.java
+++ app/src/main/java/br/com/equipe23/divugaacu/fragment/PerfilFragment.java
@@ -1,15 +1,24 @@
 package br.com.equipe23.divugaacu.fragment;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import br.com.equipe23.divugaacu.R;
 
 public class PerfilFragment extends Fragment {
+
+    private TextView textNomePerfil, textProfissaoPerfil, textMiniBioPerfil, textRua, textBairro, textNumero, textIrParaLocal;
+    private Button buttonInstagram, buttonWhatsapp;
+    private ImageView imageViewFotoPerfil;
 
     public PerfilFragment() {
         // Required empty public constructor
@@ -39,6 +48,27 @@ public class PerfilFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         View rootView = inflater.inflate(R.layout.fragment_perfil, container, false);
+
+        iniciarComponentes(rootView);
+
+        buttonWhatsapp.setOnClickListener(view -> {
+
+            Log.i("teste","foi");
+        });
+
         return rootView;
+    }
+
+    private  void iniciarComponentes(View rootView) {
+        textNomePerfil = rootView.findViewById(R.id.textNomePerfil);
+        textProfissaoPerfil = rootView.findViewById(R.id.textProfissaoPerfil);
+        textMiniBioPerfil = rootView.findViewById(R.id.textMiniBioPerfil);
+        textRua = rootView.findViewById(R.id.textRua);
+        textBairro = rootView.findViewById(R.id.textBairro);
+        textNumero = rootView.findViewById(R.id.textNumero);
+        textIrParaLocal = rootView.findViewById(R.id.textIrParaLocal);
+        buttonInstagram = rootView.findViewById(R.id.buttonInstagram);
+        buttonWhatsapp = rootView.findViewById(R.id.buttonWhatsapp);
+        imageViewFotoPerfil = rootView.findViewById(R.id.imageViewFotoPerfil);
     }
 }
