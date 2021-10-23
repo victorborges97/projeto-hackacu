5c0eca34ccf6526dff2656a001b454489e285b69
Patrick <patrickrafael05@gmail.com>
2021-10-23 12:07:40 -0300

    Pagina de ajuda

--- app/src/main/java/br/com/equipe23/divugaacu/fragment/HelpFragment.java
+++ app/src/main/java/br/com/equipe23/divugaacu/fragment/HelpFragment.java
@@ -1,15 +1,24 @@
 package br.com.equipe23.divugaacu.fragment;
 
+import android.animation.LayoutTransition;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.transition.AutoTransition;
+import androidx.transition.Transition;
+import androidx.transition.TransitionManager;
 
 import br.com.equipe23.divugaacu.R;
 
 public class HelpFragment extends Fragment {
+
+    TextView detailText;
+    LinearLayout layout;
 
     public HelpFragment() {
         // Required empty public constructor
@@ -39,6 +48,26 @@ public class HelpFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         View rootView = inflater.inflate(R.layout.fragment_help, container, false);
+        iniciarComponentes(rootView);
         return rootView;
     }
+
+    public void iniciarComponentes(View rootView) {
+
+        detailText = rootView.findViewById(R.id.details);
+        layout = rootView.findViewById(R.id.layoutTeste);
+        layout.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
+
+
+    }
+
+    public void expandir(View view) {
+        int v = (detailText.getVisibility() == View.GONE) ? View.VISIBLE : View.GONE;
+
+        TransitionManager.beginDelayedTransition(layout, new AutoTransition());
+        detailText.setVisibility(v);
+
+
+    }
+
 }
