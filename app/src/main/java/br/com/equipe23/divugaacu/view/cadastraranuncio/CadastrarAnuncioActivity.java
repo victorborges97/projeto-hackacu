d04b112eab53c6339b80b86feb9d6a9b0a308692
Leonardo Pimentel <leozito4532@gmail.com>
2021-10-23 18:54:56 -0300

    Correções

--- app/src/main/java/br/com/equipe23/divugaacu/view/cadastraranuncio/CadastrarAnuncioActivity.java
+++ app/src/main/java/br/com/equipe23/divugaacu/view/cadastraranuncio/CadastrarAnuncioActivity.java
@@ -1,16 +1,38 @@
 package br.com.equipe23.divugaacu.view.cadastraranuncio;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 import br.com.equipe23.divugaacu.R;
 
 public class CadastrarAnuncioActivity extends AppCompatActivity {
 
+    private TextInputEditText textInputEditTextTítulo, textInputEditTextPreco, textInputEditTextCidade, textInputEditTextEndereco, textInputEditTextDescricao,
+            textInputEditTextWhatsapp, textInputEditTextInstagram;
+    private AlertDialog dialog;
+    private Button buttonCadastrarAnuncio;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_anuncio);
+
+        iniciarComponentes();
+    }
+
+    public void iniciarComponentes() {
+        textInputEditTextTítulo = findViewById(R.id.textInputEditTextTítulo);
+        textInputEditTextPreco = findViewById(R.id.textInputEditTextPreco);
+        textInputEditTextCidade = findViewById(R.id.textInputEditTextCidade);
+        textInputEditTextEndereco = findViewById(R.id.textInputEditTextEndereco);
+        textInputEditTextDescricao = findViewById(R.id.textInputEditTextDescricao);
+        textInputEditTextWhatsapp = findViewById(R.id.textInputEditTextWhatsapp);
+        textInputEditTextInstagram = findViewById(R.id.textInputEditTextInstagram);
+        buttonCadastrarAnuncio = findViewById(R.id.buttonCadastrarAnuncio);
     }
 }
