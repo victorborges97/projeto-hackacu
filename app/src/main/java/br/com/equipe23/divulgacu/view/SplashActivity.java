fe40acd6d6d7c46c97e54770adf86f9b4220ebb9
Patrick <patrickrafael05@gmail.com>
2021-10-24 15:03:48 -0300

    Mudando ordem das activitys

--- app/src/main/java/br/com/equipe23/divulgacu/view/SplashActivity.java
+++ app/src/main/java/br/com/equipe23/divulgacu/view/SplashActivity.java
@@ -22,7 +22,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(getBaseContext(),LoginActivity.class));
+                startActivity(new Intent(getBaseContext(),MainActivity.class));
                 finish();
             }
         },3000);
