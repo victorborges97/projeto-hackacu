15a1983d79e7cb8ab1c9b689b50faa494d08f050
Patrick <patrickrafael05@gmail.com>
2021-10-23 22:38:54 -0300

    Adicionando mais perguntas

--- app/src/main/java/br/com/equipe23/divugaacu/fragment/HelpFragment.java
+++ app/src/main/java/br/com/equipe23/divugaacu/fragment/HelpFragment.java
@@ -51,18 +51,28 @@ public class HelpFragment extends Fragment {
     }
 
     public void criarPerguntas(){
-<<<<<<< HEAD
 
-        perguntas = new Perguntas("Como entar com contato?", "Clicando no botão do WhatsApp, você entrará diretamente em contato com o fornecedor, podendo falar diretamente com ele para realizar seu negócio.");
+
+        perguntas = new Perguntas("Como entrar com contato?", "Clicando no botão do WhatsApp, você entrará diretamente em contato com o fornecedor, podendo falar diretamente com ele para realizar seu negócio.");
         this.listaPerguntas.add(perguntas);
 
         perguntas = new Perguntas("Como é feito o cadastro da meu negócio?", "Após entrar no aplicativo, o usuário terá apenas que clicar no botão de adicionar no topo da tela, assim só é necessário preecher os dados e apertar o botão cadastrar, pronto, Registrado!");
-=======
-        perguntas = new Perguntas("Como funciona a comunicação cliente/fornecedor?", "Da forma mais simples possivel, o cliente procura o serviço que deseja e assim começa sua negociação, diretamente com o fornecedor, uma relação inter-pessoal, assim os  dois entram em acrodo e um negócio é feito");
         this.listaPerguntas.add(perguntas);
-        perguntas = new Perguntas("Como funciona a comunicação cliente/fornecedor?", "Da forma mais simples possivel, o cliente procura o serviço que deseja e assim começa sua negociação, diretamente com o fornecedor, uma relação inter-pessoal, assim os  dois entram em acrodo e um negócio é feito");
->>>>>>> 572b414912e1a03c75236dba17c2145eedda2b18
+
+        perguntas = new Perguntas("Como encontro o serviço que quero?", "Em qualquer uma das telas, na barra em baixo, ao clicar na lupa, será possivel pesquisar qual tipo de serviço o usuário deseja.");
         this.listaPerguntas.add(perguntas);
+
+        perguntas = new Perguntas("Como escolher outra cidade?", "Na pagina principal, apenas arrastando para o lado a cidade já troca.");
+        this.listaPerguntas.add(perguntas);
+
+        perguntas = new Perguntas("Como ver ou alterar meu usuário?", "Na barra em baixo, ao apertar no icone da pessoa, a aba de usuários é aberta, onde tem todas suas informações da sua conta.");
+        this.listaPerguntas.add(perguntas);
+
+        perguntas = new Perguntas("Como sair do aplicativo?", "Na barra em baixo, ao apertar no icone da pessoa, a aba de usuários é aberta, onde tem todas suas informações da sua conta.");
+        this.listaPerguntas.add(perguntas);
+
+
+
     }
 
 }
