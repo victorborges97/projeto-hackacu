7aaf73f33e9c7d14592f64340f566c0cdbbe7ea5
Patrick <patrickrafael05@gmail.com>
2021-10-23 22:42:09 -0300

    Tirando erro

--- app/src/main/java/br/com/equipe23/divugaacu/fragment/HelpFragment.java
+++ app/src/main/java/br/com/equipe23/divugaacu/fragment/HelpFragment.java
@@ -51,16 +51,16 @@ public class HelpFragment extends Fragment {
     }
 
     public void criarPerguntas(){
-<<<<<<< HEAD
+
 
 
         perguntas = new Perguntas("Como entrar com contato?", "Clicando no botão do WhatsApp, você entrará diretamente em contato com o fornecedor, podendo falar diretamente com ele para realizar seu negócio.");
-=======
+
         perguntas = new Perguntas("Como entar com contato?", "Clicando no botão do WhatsApp, você entrará diretamente em contato com o fornecedor, podendo falar diretamente com ele para realizar seu negócio.");
->>>>>>> 9dbf1aff853cda5f4181834e57d3fd4e809ad8a1
+
         this.listaPerguntas.add(perguntas);
         perguntas = new Perguntas("Como é feito o cadastro da meu negócio?", "Após entrar no aplicativo, o usuário terá apenas que clicar no botão de adicionar no topo da tela, assim só é necessário preecher os dados e apertar o botão cadastrar, pronto, Registrado!");
-<<<<<<< HEAD
+
         this.listaPerguntas.add(perguntas);
 
         perguntas = new Perguntas("Como encontro o serviço que quero?", "Em qualquer uma das telas, na barra em baixo, ao clicar na lupa, será possivel pesquisar qual tipo de serviço o usuário deseja.");
@@ -70,10 +70,10 @@ public class HelpFragment extends Fragment {
         this.listaPerguntas.add(perguntas);
 
         perguntas = new Perguntas("Como ver ou alterar meu usuário?", "Na barra em baixo, ao apertar no icone da pessoa, a aba de usuários é aberta, onde tem todas suas informações da sua conta.");
-=======
+
         perguntas = new Perguntas("Como funciona a comunicação cliente/fornecedor?", "Da forma mais simples possivel, o cliente procura o serviço que deseja e assim começa sua negociação, diretamente com o fornecedor, uma relação inter-pessoal, assim os  dois entram em acrodo e um negócio é feito");
         this.listaPerguntas.add(perguntas);
->>>>>>> 9dbf1aff853cda5f4181834e57d3fd4e809ad8a1
+
         this.listaPerguntas.add(perguntas);
 
         perguntas = new Perguntas("Como sair do aplicativo?", "Na barra em baixo, ao apertar no icone da pessoa, a aba de usuários é aberta, onde tem todas suas informações da sua conta.");
